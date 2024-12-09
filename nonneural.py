cedc35590b51515ac6676accb63d4e04c65d0174
Timmald-School <118050114+Timmald-School@users.noreply.github.com>
2024-12-09 03:55:16 -0500

    starting verb thing

--- nonneural.py
+++ nonneural.py
@@ -14,6 +14,7 @@ from glob import glob
 import pickle
 import tqdm
 
+allWordsReader = open("Latin_stuff/lat.dev","r").readlines()
 
 def hamming(s,t):
     return sum(1 for x,y in zip(s,t) if x != y)
@@ -133,14 +134,72 @@ def apply_best_rule(lemma, msd, allprules, allsrules):
     base = "<" + lemma + ">"
     if msd not in allprules and msd not in allsrules:
         return lemma # Haven't seen this inflection, so bail out
+    
+    #THE BELOW IS MY ADDITION FOR THE 1ST-3RD CONJ AMBIGUITY
+    elif msd.startswith("V;") and lemma.endswith("ō"):
+        noSuff = lemma[:-1]
+        for word in allWordsReader:
+            lemma2,msd2,form = word.replace("\n","").split("\t")
+            if lemma2 == lemma and "NFIN" in msd2:#LOOK FOR INFINITIVE
+                break
+        if form == 'Icarūsa':
+            pass 
+        elif form.endswith("āre"):#1st conj
+            conjDict = {"V;IND;ACT;PRS;1;SG":("ō","ō"),
+                        "V;IND;ACT;PRS;2;SG":("ō","ās"),
+                        "V;IND;ACT;PRS;3;SG":("ō","at"),
+                        "V;IND;ACT;PRS;1;PL":("ō","āmus"),
+                        "V;IND;ACT;PRS;2;PL":("ō","ātis"),
+                        "V;IND;ACT;PRS;3;PL":("ō","ant"),
+                        "V;IND;ACT;PST;IPFV;1;SG":("ō","ābam"),
+                        "V;IND;ACT;PST;IPFV;2;SG":("ō","ābās"),
+                        "V;IND;ACT;PST;IPFV;3;SG":("ō","ābat"),
+                        "V;IND;ACT;PST;IPFV;1;PL":("ō","āmus"),
+                        "V;IND;ACT;PST;IPFV;2;PL":("ō","ātis"),
+                        "V;IND;ACT;PST;IPFV;3;PL":("ō","bant"),
+                        "V;IND;ACT;FUT;1;SG":("ō","ābō"),
+                        "V;IND;ACT;FUT;2;SG":("ō","ābis"),
+                        "V;IND;ACT;FUT;3;SG":("ō","ābit"),
+                        "V;IND;ACT;FUT;1;PL":("ō","ābimus"),
+                        "V;IND;ACT;FUT;2;PL":("ō","ābitis"),
+                        "V;IND;ACT;FUT;3;PL":("ō","ābunt"),
+                        "V;SBJV;ACT;PRS;1;SG":("ō","em"),
+                        "V;SBJV;ACT;PRS;2;SG":("ō","ēs"),
+                        "V;SBJV;ACT;PRS;3;SG":("ō","et"),
+                        "V;SBJV;ACT;PRS;1;PL":("ō","ēmus"),
+                        "V;SBJV;ACT;PRS;2;PL":("ō","ētis"),
+                        "V;SBJV;ACT;PRS;3;PL":("ō","ent"),
+                        "V;SBJV;ACT;PST;IPFV;1;SG":("ō","ārem"),
+                        "V;SBJV;ACT;PST;IPFV;2;SG":("ō","ārēs"),
+                        "V;SBJV;ACT;PST;IPFV;3;SG":("ō","āret"),
+                        "V;SBJV;ACT;PST;IPFV;1;PL":("ō","ārēmus"),
+                        "V;SBJV;ACT;PST;IPFV;2;PL":("ō","ārētis"),
+                        "V;SBJV;ACT;PST;IPFV;3;PL":("ō","ārent"),
+                        "V;IMP;ACT;PRS;2;SG":("ō","ā"),
+                        "V;IMP;ACT;PRS;2;PL":("ō","āte"),
+                        "V;IMP;ACT;FUT;2;SG":("ō","ātō"),
+                        "V;IMP;ACT;FUT;3;SG":("ō","ātō"),
+                        "V;IMP;ACT;FUT;2;PL":("ō","ātōte"),
+                        "V;IMP;ACT;FUT;3;PL":("ō","antō"),
+                        "V;NFIN;ACT;PRS":("ō","āre"),
+                        "V;V.PTCP;ACT;PRS":("ō","āns"),
+                        "V;V.MSDR;GEN":("ō","andī"),
+                        "V;V.MSDR;DAT":("ō","andō"),
+                        "V;V.MSDR;ACC":("ō","andum"),
+                        "V;V.MSDR;ABL":("ō","andō"),
+                    }
+            return noSuff+conjDict[msd][1]
+        elif form.endswith("ēre"):#3rd conj
+            pass
+                
 
-    if msd in allsrules:
+    elif msd in allsrules:
         applicablerules = [(x[0],x[1],y) for x,y in allsrules[msd].items() if x[0] in base]
         if applicablerules:
             bestrule = max(applicablerules, key = lambda x: (len(x[0]), x[2], len(x[1])))
             base = base.replace(bestrule[0], bestrule[1])
 
-    if msd in allprules:
+    elif msd in allprules:
         applicablerules = [(x[0],x[1],y) for x,y in allprules[msd].items() if x[0] in base]
         if applicablerules:
             bestrule = max(applicablerules, key = lambda x: (x[2]))
